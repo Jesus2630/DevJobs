f2a7a798372aee3e900a9fb6e3a85a39013db9d0
Jesus Farias <jesus.fg2630@gmail.com>
2022-10-03 21:25:09 -0300

    Hasta acá todo bien, todo correcto, y yo que me alegro

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -4,16 +4,40 @@ const { body, validationResult } = require('express-validator');
 const shortid = require('shortid');
 const multer = require('multer');
 
-exports.subirImagen = (req,res) =>{
+
+//Configuracion Multer
+const configuracionMulter = {
+    storage : multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, __dirname + '../../public/uploads/perfiles')
+        },
+        filename: function (req, file, cb) {
+            const extension = file.mimetype.split('/')[1]
+            cb(null, `${shortid.generate()}.${extension}`)
+        }
+      }),
+      fileFilter(req,file,cb){
+        if( file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+            cb(null, true);
+        }else{
+            cb(null, false);
+        }
+      }
+} 
+
+const upload = multer (configuracionMulter).single('imagen');
+
+exports.subirImagen = (req,res,next) =>{
     upload(req,res, function(error){
         if(error instanceof multer.MulterError){
-            
+            return next();
         }
     })
+    next();
 }
 
 
-
+//Crear cuenta
 exports.formCrearCuenta = (req,res) =>{
      res.render('crear-cuenta', {
         nombrePagina: 'Crea tu cuenta en Enjobs',
